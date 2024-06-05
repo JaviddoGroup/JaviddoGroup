508b1e66d86e5b75670e4b73d20d4a1bce8d10f8
JaviddoGroup <154234110+JaviddoGroup@users.noreply.github.com>
2024-06-05 14:33:17 +0400

    IOS Fix

--- scripts/video-overlay.js
+++ scripts/video-overlay.js
@@ -7,3 +7,18 @@ document.addEventListener('DOMContentLoaded', function () {
         document.documentElement.classList.add('html-active-scroll'); // Добавить класс к <html> элементу
     });
 });
+document.addEventListener('DOMContentLoaded', function () {
+    var video = document.getElementById('preload-video');
+
+    // Проверяем, является ли устройство iOS
+    var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+
+    // Если это устройство iOS, воспроизводим видео по клику
+    if (iOS) {
+        video.addEventListener('click', function () {
+            video.play();
+        });
+    } else { // В противном случае, на других устройствах воспроизводим видео автоматически
+        video.play();
+    }
+});

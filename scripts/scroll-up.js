ecb200c5a3827830789273e74861c573a24896f4
JaviddoGroup <154234110+JaviddoGroup@users.noreply.github.com>
2024-06-05 11:05:23 +0400

    Scroll Up button and response

---
+++ scripts/scroll-up.js
@@ -0,0 +1,25 @@
+// Get the button and progress circle
+let scrollUpBtn = document.getElementById("scroll-ele");
+let progressCircle = document.getElementById("progress-circle");
+
+// Show the button when the user scrolls down 20px from the top of the document
+window.onscroll = function () {
+    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollPercentage = (scrollTop / scrollHeight) * 100;
+
+    // Update progress circle
+    progressCircle.style.setProperty('--progress', scrollPercentage * 3.6 + 'deg');
+
+    if (scrollTop > 20) {
+        scrollUpBtn.classList.add("show");
+    } else {
+        scrollUpBtn.classList.remove("show");
+    }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+document.getElementById('scroll-up').addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});

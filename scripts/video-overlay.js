b46dcffc68be60e06f9da2fd210c5a6a1b246d18
JaviddoGroup <154234110+JaviddoGroup@users.noreply.github.com>
2024-06-05 18:40:28 +0400

    Delete old code

--- scripts/video-overlay.js
+++ scripts/video-overlay.js
@@ -1,28 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     var upperLogoText = document.querySelectorAll('.upper-logo-text img');
-//     var underLogoText = document.querySelectorAll('.unnder-logo-text img');
-//     var animationLogoLeft = document.querySelector('.animation-logo-left');
-//     var middleLogoLine = document.querySelector('.middle-logo-line');
-//     var overlay = document.querySelector('.animation-overlay');
-
-//     // Функция для последовательной анимации элементов
-//     function animateSequentially(elements, delay) {
-//         elements.forEach((el, index) => {
-//             el.style.animationDelay = `${delay * index}s`;
-//         });
-//     }
-
-//     // Анимация элементов верхнего и нижнего текста
-//     animateSequentially(upperLogoText, 0.5);
-//     animateSequentially(underLogoText, 0.5);
-
-//     // Убираем overlay и показываем основной контент через 6 секунд (время анимации)
-//     setTimeout(function () {
-//         overlay.style.display = 'none';
-//         document.documentElement.classList.add('html-active-scroll'); // Включаем прокрутку
-//         document.getElementById('main-content').style.display = 'block'; // Показываем основной контент
-//     }, 6000); // Время на завершение всех анимаций
-// });
 document.addEventListener('DOMContentLoaded', function () {
     var gif = document.getElementById('preload-gif');
     var videoOverlay = document.getElementById('video-overlay');
@@ -46,6 +21,6 @@ document.addEventListener('DOMContentLoaded', function () {
         videoOverlay.style.display = 'none';
         document.documentElement.classList.add('html-active-scroll'); // Включаем прокрутку
         document.getElementById('main-content').style.display = 'block'; // Показываем основной контент
-    }, 4480); // Время на завершение всех анимаций
+    }, 4400); // Время на завершение всех анимаций
 });
 

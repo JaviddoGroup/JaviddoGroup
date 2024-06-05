0b8fa609df1ab38659ab42e078d1ece9ccb8efde
JaviddoGroup <154234110+JaviddoGroup@users.noreply.github.com>
2024-06-05 17:55:33 +0400

    Try fix mobile gif

--- scripts/video-overlay.js
+++ scripts/video-overlay.js
@@ -27,9 +27,25 @@ document.addEventListener('DOMContentLoaded', function () {
     var gif = document.getElementById('preload-gif');
     var videoOverlay = document.getElementById('video-overlay');
 
+    function setGifSource() {
+        if (window.innerWidth <= 600) {
+            gif.src = './media/video-pre-load-mobile.gif';
+        } else {
+            gif.src = './media/video-pre-load.gif';
+        }
+    }
+
+    // Установить исходный путь к GIF при загрузке страницы
+    setGifSource();
+
+    // Обновлять путь к GIF при изменении размера окна
+    window.addEventListener('resize', setGifSource);
+
+    // Убираем overlay и показываем основной контент через 6 секунд (время анимации)
     setTimeout(function () {
-        videoOverlay.style.display = 'none'; // Скрыть GIF оверлей
-        document.documentElement.classList.add('html-active-scroll'); // Добавить класс к <html> элементу
-        document.getElementById('main-content').style.display = 'block'; // Показать основной контент
-    }, 4480); // Таймер на 5 секунд
+        videoOverlay.style.display = 'none';
+        document.documentElement.classList.add('html-active-scroll'); // Включаем прокрутку
+        document.getElementById('main-content').style.display = 'block'; // Показываем основной контент
+    }, 4480); // Время на завершение всех анимаций
 });
+

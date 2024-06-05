91f07fe9a4862b9c31c0502286d5933d52cf5e68
JaviddoGroup <154234110+JaviddoGroup@users.noreply.github.com>
2024-06-05 17:30:13 +0400

    Try another way

--- scripts/video-overlay.js
+++ scripts/video-overlay.js
@@ -1,24 +1,35 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     var upperLogoText = document.querySelectorAll('.upper-logo-text img');
+//     var underLogoText = document.querySelectorAll('.unnder-logo-text img');
+//     var animationLogoLeft = document.querySelector('.animation-logo-left');
+//     var middleLogoLine = document.querySelector('.middle-logo-line');
+//     var overlay = document.querySelector('.animation-overlay');
+
+//     // Функция для последовательной анимации элементов
+//     function animateSequentially(elements, delay) {
+//         elements.forEach((el, index) => {
+//             el.style.animationDelay = `${delay * index}s`;
+//         });
+//     }
+
+//     // Анимация элементов верхнего и нижнего текста
+//     animateSequentially(upperLogoText, 0.5);
+//     animateSequentially(underLogoText, 0.5);
+
+//     // Убираем overlay и показываем основной контент через 6 секунд (время анимации)
+//     setTimeout(function () {
+//         overlay.style.display = 'none';
+//         document.documentElement.classList.add('html-active-scroll'); // Включаем прокрутку
+//         document.getElementById('main-content').style.display = 'block'; // Показываем основной контент
+//     }, 6000); // Время на завершение всех анимаций
+// });
 document.addEventListener('DOMContentLoaded', function () {
-    var video = document.getElementById('preload-video');
+    var gif = document.getElementById('preload-gif');
     var videoOverlay = document.getElementById('video-overlay');
 
-    video.addEventListener('ended', function () {
-        videoOverlay.style.display = 'none'; // Скрыть видео оверлей
+    setTimeout(function () {
+        videoOverlay.style.display = 'none'; // Скрыть GIF оверлей
         document.documentElement.classList.add('html-active-scroll'); // Добавить класс к <html> элементу
-    });
-});
-document.addEventListener('DOMContentLoaded', function () {
-    var video = document.getElementById('preload-video');
-
-    // Проверяем, является ли устройство iOS
-    var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-
-    // Если это устройство iOS, воспроизводим видео по клику
-    if (iOS) {
-        video.addEventListener('click', function () {
-            video.play();
-        });
-    } else { // В противном случае, на других устройствах воспроизводим видео автоматически
-        video.play();
-    }
+        document.getElementById('main-content').style.display = 'block'; // Показать основной контент
+    }, 4480); // Таймер на 5 секунд
 });

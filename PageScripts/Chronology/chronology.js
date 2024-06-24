83ba8eea523c4835b1ce21dc423152160e997611
JaviddoGroup <154234110+JaviddoGroup@users.noreply.github.com>
2024-06-24 12:32:29 +0400

    Chronology Responsive Add

--- PageScripts/Chronology/chronology.js
+++ PageScripts/Chronology/chronology.js
@@ -14,3 +14,23 @@ document.addEventListener('scroll', function () {
         }
     });
 });
+
+
+
+
+document.addEventListener('scroll', function () {
+    const cardsMobile = document.querySelectorAll('.timeline-card-mobile');
+
+    cardsMobile.forEach(cardMobi => {
+        const rect = cardMobi.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+
+        // Выбираем момент, когда карточка должна появиться на экране
+        // В данном случае, когда она приблизительно на 50% видимости
+        if (rect.top < windowHeight * 0.5 && rect.bottom > 0) {
+            cardMobi.classList.add('timeline-card-mobile-visible-card');
+        } else {
+            cardMobi.classList.remove('timeline-card-mobile-visible-card');
+        }
+    });
+});

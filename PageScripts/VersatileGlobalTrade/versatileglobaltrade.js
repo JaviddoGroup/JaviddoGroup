86f01fa72e1d6d20a1276fc13a79f45f852d0bb5
JaviddoGroup <154234110+JaviddoGroup@users.noreply.github.com>
2024-07-03 14:45:43 +0400

    New page Versatile Global Trade

---
+++ PageScripts/VersatileGlobalTrade/versatileglobaltrade.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Элементы с классом 'v-g-t-animate' будут плавно появляться при загрузке страницы
+    const animateElements = document.querySelectorAll('.v-g-t-animate');
+    animateElements.forEach(element => {
+        element.style.opacity = '1';
+    });
+});
+
+
+
+const slider = document.querySelector('.slider');
+
+function activate(e) {
+    const items = document.querySelectorAll('.item');
+    e.target.matches('.next') && slider.append(items[0])
+    e.target.matches('.prev') && slider.prepend(items[items.length - 1]);
+}
+
+document.addEventListener('click', activate, false);

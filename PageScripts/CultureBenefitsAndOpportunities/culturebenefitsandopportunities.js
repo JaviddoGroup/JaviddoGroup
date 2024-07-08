c3bbf9decc91f585f9b22265f0f2741a35411fb7
JaviddoGroup <154234110+JaviddoGroup@users.noreply.github.com>
2024-07-08 12:33:59 +0400

    Add new page

--- PageScripts/CultureBenefitsAndOpportunities/culturebenefitsandopportunities.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const reasons = document.querySelectorAll('.reason');
-    reasons.forEach(reason => {
-        reason.addEventListener('click', () => {
-            alert('Вы выбрали: ' + reason.querySelector('h3').textContent);
-        });
-    });
-});
